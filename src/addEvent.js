831489d6ee610de8b80bd3c6e46b661fdf3a1d96
Joilson Marques <joilson.marques@ebc.com.br>
2013-08-13 10:20:58 -0300

    addEvent.js: Cross-browser implementation of element.addEventListener()

---
+++ src/addEvent.js
@@ -0,0 +1,21 @@
+/*
+ * addEvent.js: Cross-browser implementation of element.addEventListener()
+ * 2013-08-13
+ *
+ * By Joilson Marques, http://joilsonmarques@github.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ *
+ * Use: addEvent("event name", elem, func);
+ */
+
+function addEvent(evnt, elem, func) {
+   if (elem.addEventListener)  // W3C DOM
+      elem.addEventListener(evnt,func,false);
+   else if (elem.attachEvent) { // IE DOM
+      elem.attachEvent("on"+evnt, func);
+   }
+   else { // No much to do
+      elem[evnt] = func;
+   }
+}
